bf89e68d940560550f32c5ecf3dc13818b03f323
Charlie Sharpsteen <source@sharpsteen.net>
2016-03-28 16:55:29 -0700

    Update Oscar dependency to 0.5.0
    This patch also removes a lot of older dependencies as they are enforced by
    installing Oscar 0.5.0.

--- Gemfile
+++ Gemfile
@@ -10,9 +10,5 @@ gem 'vagrant', :github => 'mitchellh/vagrant', :tag => 'v1.8.1'
 # simulates the action of `vagrant plugin`, which is inactive when running
 # under Bundler.
 group :plugins do
-  gem 'oscar', '>= 0.4'
-  gem 'vagrant-hosts', '>= 2.6.0'
-  gem 'vagrant-auto_network'
-  gem 'vagrant-pe_build', '>= 0.13.7'
-  gem 'vagrant-config_builder', '>= 0.15.0'
+  gem 'oscar', '>= 0.5'
 end
--- lib/puppet_debugging_kit/plugin_checks.rb
+++ lib/puppet_debugging_kit/plugin_checks.rb
@@ -16,33 +16,14 @@ module PuppetDebuggingKit
       def error_message; @error_message; end
     end
 
-    REQUIRED_HOSTS          = Gem::Version.new('2.6.0')
-    REQUIRED_OSCAR          = Gem::Version.new('0.4.0')
-    REQUIRED_PEBUILD        = Gem::Version.new('0.13.7')
-    REQUIRED_CONFIGBUILD    = Gem::Version.new('0.15.0')
+    REQUIRED_OSCAR          = Gem::Version.new('0.5.0')
 
     # Performs sanity checks on required plugins.
     def self.run
-      vagrant_version        = Gem::Version.new(Vagrant::VERSION)
       oscar_version          = Gem::Version.new(Oscar::VERSION)
-      hosts_version          = Gem::Version.new(VagrantHosts::VERSION)
-      pebuild_version        = Gem::Version.new(PEBuild::VERSION)
-      configbuild_version    = Gem::Version.new(ConfigBuilder::VERSION)
 
       if oscar_version < REQUIRED_OSCAR
         raise DebugKitBadVersion.new('oscar', REQUIRED_OSCAR, oscar_version)
-      end
-
-      if pebuild_version < REQUIRED_PEBUILD
-        raise DebugKitBadVersion.new('vagrant-pe_build', REQUIRED_PEBUILD, pebuild_version)
-      end
-
-      if configbuild_version < REQUIRED_CONFIGBUILD
-        raise DebugKitBadVersion.new('vagrant-config_builder', REQUIRED_CONFIGBUILD, configbuild_version)
-      end
-
-      if vagrant_version >= Gem::Version.new('1.5.0') && hosts_version < REQUIRED_HOSTS
-        raise DebugKitBadVersion.new('vagrant-hosts', REQUIRED_HOSTS, hosts_version)
       end
     end
   end
