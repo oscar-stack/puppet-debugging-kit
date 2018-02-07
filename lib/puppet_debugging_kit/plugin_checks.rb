db098623f7cdb8b47a9fa8f1c2e4ab4b02c5ca89
Charlie Sharpsteen <source@sharpsteen.net>
2018-02-06 17:02:07 -0800

    Handle removal of Vagrant boxes from Atlas
    Hosting of Vagrant boxes on atlas.hashicorp.com has moved to vagrantcloud.com
    and this functionality of atlas has been decommissioned, apparently some time
    last week. This means that VM creation fails when a box has to be downloaded.
    
    Vagrant migrated to using vagrantcloud.com as the default server URL in version
    1.9.6, but Oscar is currently stuck on 1.8.7 due to testing issues. This commit
    adds a conditional to the Debug Kit plugin checks run during startup that
    patches in an updated URL if the Vagrant version is too old.
    
    Fixes Sharpie/puppet-debugging-kit#19.

--- lib/puppet_debugging_kit/plugin_checks.rb
+++ lib/puppet_debugging_kit/plugin_checks.rb
@@ -16,13 +16,23 @@ module PuppetDebuggingKit
     end
 
     REQUIRED_OSCAR          = Gem::Requirement.new('>= 0.5.3')
+    ATLAS_CUTOFF_VERSION    = Gem::Requirement.new('< 1.9.6')
 
     # Performs sanity checks on required plugins.
     def self.run
       oscar_version          = Gem::Version.new(Oscar::VERSION)
+      vagrant_version        = Gem::Version.new(Vagrant::VERSION)
 
       unless REQUIRED_OSCAR.satisfied_by?(oscar_version)
         raise DebugKitBadVersion.new('oscar', REQUIRED_OSCAR.to_s, oscar_version)
+      end
+
+      # Vagrant boxes were hosted by atlas.hashicorp.com but have since
+      # migrated to vagrantcloud.com. The Atlas service was de-commissioned
+      # in early 2018, so older Vagrant versions need their default URL
+      # monkeypatched.
+      if ATLAS_CUTOFF_VERSION.satisfied_by?(vagrant_version)
+        Vagrant::DEFAULT_SERVER_URL.replace('https://vagrantcloud.com')
       end
     end
   end
